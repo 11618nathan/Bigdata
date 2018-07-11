f95f37f0ef88c28ad8856806ad4f7b5762f3b016
11618nathan <11618nathan@gmail.com>
2018-07-11 17:23:29 +0900

    Upload

---
+++ googleVis/googleVis Pie chart.R
@@ -0,0 +1,14 @@
+setwd("c:/bigdata")
+
+# googleVis 라이브러리 설치 및 호출
+install.packages("googleVis")
+library(googleVis)
+
+# 변수 지정    val1=해당 값1   val2=해당 값2
+df=data.frame(database=c("nt", "re", "mo"), val1=c(28, 12, 24), val2=c(11, 15, 33))
+
+# Pie차트 지정
+Pie <- gvisPieChart(df)
+
+# plot 안에 chart 지정 변수 불러오기
+plot(Pie)
