d32b2ba93e818ebbbc455b3c80a7e865a1f5eeec
11618nathan <11618nathan@gmail.com>
2018-08-08 23:07:28 +0900

    Upload

---
+++ basic/big googleVis2 library.R
@@ -0,0 +1,19 @@
+setwd("c:/bigdata")
+
+#googleVis 라이브러리 설치 및 호출
+install.packages("googleVis")
+library(googleVis)
+
+#데이터 파일 loc에 불러들임. 
+loc <- read.csv("library.csv",header=T)
+loc
+
+#변수 이름과 파일 이름 같이.
+library <- gvisMap(loc, "LATLON" , "name",
+                  options=list(showTip=TRUE, showLine=TRUE, enableScrollWheel=TRUE,
+                               mapType='normal', useMapTypeControl=TRUE,
+                               width=1000,height=400))
+
+#플롯 차트 변수 이름 같이
+plot(library)
+
