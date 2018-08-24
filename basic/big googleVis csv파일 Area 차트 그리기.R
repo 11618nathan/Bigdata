29345df0136a96e75d03bf0de4d7adf31b393988
11618nathan <11618nathan@gmail.com>
2018-08-24 22:08:57 +0900

    Upload

---
+++ basic/big googleVis csv파일 Area 차트 그리기.R
@@ -0,0 +1,15 @@
+setwd("c:/bigdata")
+
+#googleVis 라이브러리 설치 및 호출
+install.packages("googleVis")
+library(googleVis)
+
+#csv파일 읽기 변수 설정
+pop <- read.csv("Population.csv", header = T)
+pop
+
+#Pie차트 지정
+Area <- gvisAreaChart(pop, options = list(height=500, weight=500))
+
+#plot 차트 그리기
+plot(Area)
